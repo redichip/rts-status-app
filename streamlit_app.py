683790b688b683c70eed3f6d35443da21b07c25f
redichip <scott@redichip.com>
2025-01-14 20:09:34 +0000

    hide index

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ from st_files_connection import FilesConnection
 conn = st.connection('gcs', type=FilesConnection)
 df = conn.read("520885133717268/Results.csv", input_format="csv", ttl=600)
 st.title('RTS View')
-st.dataframe(df, use_container_width=True)
+st.dataframe(df[['Ticker', 'Position', 'CODProfit']], use_container_width=True)
