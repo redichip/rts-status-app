a8e1c6f357b3ef681e969d283e3960ca6ac72713
redichip <scott@redichip.com>
2025-01-14 20:12:05 +0000

    hide the index

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,6 @@ from st_files_connection import FilesConnection
 conn = st.connection('gcs', type=FilesConnection)
 df = conn.read("520885133717268/Results.csv", input_format="csv", ttl=600)
 st.title('RTS View')
-df = df.reset_index(drop=True)
 st.dataframe(df, use_container_width=True)
+st.dataframe(df.style.hide(axis='index'))
+
