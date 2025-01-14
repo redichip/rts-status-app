8afba6286ac0b4b0fdf4906f499aef40adc5c3a2
redichip <scott@redichip.com>
2025-01-14 20:07:18 +0000

    use title function

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ from st_files_connection import FilesConnection
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.connection('gcs', type=FilesConnection)
 df = conn.read("520885133717268/Results.csv", input_format="csv", ttl=600)
-st.write('Title')
+st.title('RTS View')
 st.dataframe(df, use_container_width=True)
